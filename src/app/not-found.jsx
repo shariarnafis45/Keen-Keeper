4c8294fa2c7a8d36c661aa8839695e3930837ae2
shariarnafis45 <nafisshahworkmail@gmail.com>
2026-04-19 15:44:37 +0600

    added 404 error page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div className='min-h-screen flex flex-col justify-center items-center'>
+            <h2 className='text-9xl font-bold text-[#244D3F]'>404</h2>
+            <p className='text-2xl mt-3 font-medium'>Page Not Found</p>
+        </div>
+    );
+};
+
+export default ErrorPage;
