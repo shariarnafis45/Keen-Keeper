ef9b08404eedca6bc34b2ea50bbed9a0f79dc74e
shariarnafis45 <nafisshahworkmail@gmail.com>
2026-04-20 01:03:14 +0600

    added filter functionality in timeline

--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -2,22 +2,39 @@
 
 import ActivityCard from "@/components/timeline/ActivityCard";
 import { FriendContext } from "@/context/FriendContext";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 const TimelinePage = () => {
   const { timeline } = useContext(FriendContext);
+  const [filter, setFilter] = useState("all");
+  const filteredTimeline =
+    filter === "all"
+      ? timeline
+      : timeline.filter((data) => data.actionType === filter);
 
   return (
     <div className="bg-[#F8FAFC] py-10 px-5">
       <div className="max-w-7xl mx-auto">
         <h2 className="text-5xl font-bold">Timeline</h2>
-        {timeline.length === 0 ? (
+        <div className="my-5">
+          <select
+            onChange={(e) => setFilter(e.target.value)}
+            className="select focus:border-none outline-none"
+          >
+            <option disabled={true}>Filter timeline</option>
+            <option value="all">All</option>
+            <option value="call">Call</option>
+            <option value="text">Text</option>
+            <option value="video">Video</option>
+          </select>
+        </div>
+        {filteredTimeline.length === 0 ? (
           <div className="min-h-[50vh] flex justify-center items-center bg-white mt-7 shadow-sm">
             <h2 className="text-2xl font-medium">No Data Available</h2>
           </div>
         ) : (
           <div className="mt-7 space-y-3">
-            {timeline.map((data, i) => (
+            {filteredTimeline.map((data, i) => (
               <ActivityCard key={i} data={data} />
             ))}
           </div>
