78000085b701648532e183497cbef9f44ffeed2f
shariarnafis45 <nafisshahworkmail@gmail.com>
2026-04-19 15:54:44 +0600

    handle empty timeline data

--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -1,25 +1,30 @@
-'use client'
+"use client";
 
-import ActivityCard from '@/components/timeline/ActivityCard';
-import { FriendContext } from '@/context/FriendContext';
-import React, { useContext } from 'react';
+import ActivityCard from "@/components/timeline/ActivityCard";
+import { FriendContext } from "@/context/FriendContext";
+import React, { useContext } from "react";
 
 const TimelinePage = () => {
-    const {timeline} = useContext(FriendContext)
-    
-    
-    return (
-        <div className='bg-[#F8FAFC] py-10 px-5'>
-            <div className='max-w-7xl mx-auto'>
-                <h2 className='text-5xl font-bold'>Timeline</h2>
-                <div className='mt-7 space-y-3'>
-                    {
-                        timeline.map((data, i) => <ActivityCard key={i} data={data}/>)
-                    }
-                </div>
-            </div>
-        </div>
-    );
+  const { timeline } = useContext(FriendContext);
+
+  return (
+    <div className="bg-[#F8FAFC] py-10 px-5">
+      <div className="max-w-7xl mx-auto">
+        <h2 className="text-5xl font-bold">Timeline</h2>
+        {timeline.length === 0 ? (
+          <div className="min-h-[50vh] flex justify-center items-center bg-white mt-7 shadow-sm">
+            <h2 className="text-2xl font-medium">No Data Available</h2>
+          </div>
+        ) : (
+          <div className="mt-7 space-y-3">
+            {timeline.map((data, i) => (
+              <ActivityCard key={i} data={data} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default TimelinePage;
